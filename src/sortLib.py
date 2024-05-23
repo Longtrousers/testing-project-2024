4df2f7f5d91dd5614b45589401310f2b9b90f4d5
Pudding Longtrousers <118670748+Longtrousers@users.noreply.github.com>
2024-05-23 16:07:38 +0300

    aaaaa

--- src/sortLib.py
+++ src/sortLib.py
@@ -1,9 +1,9 @@
-from selenium import webdriver
 from re import sub
-import selenium
-import selenium.webdriver
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_prices(url: str, title: str, driver) -> list[int]:
@@ -11,15 +11,71 @@ def get_prices(url: str, title: str, driver) -> list[int]:
     assert title in driver.title
 
     price_divs = driver.find_elements(
-        By.XPATH,
-        "//div[@class='product-list']//div[@data-test-id='price-current-price']",
-    )
-    for i in price_divs:
-        print(i.text)
-    prices = list(
-        map(
-            lambda x: int(sub("\\D+", "", x.text)),
-            price_divs,
-        )
-    )
+        By.XPATH, "//div[@class='product-list']//div[@data-test-id='price-current-price']")
+    prices = list(map(lambda x: int(sub("\\D+", "", x.text)), price_divs))
     return prices
+
+
+def get_reviews(url: str, title: str, driver) -> list[int]:
+    driver.get(url)
+    assert title in driver.title
+
+    review_divs = driver.find_elements(
+        By.XPATH, "//div[@class='product-list']//div[@data-test-id='review']//div[2]")
+    reviews = list(map(lambda x: int(sub("\\D+", "", x.text)), review_divs))
+    return reviews
+
+
+def get_sales(url: str, title: str, driver) -> list[int]:
+    driver.get(url)
+    assert title in driver.title
+
+    sales_divs = driver.find_elements(
+        By.XPATH, "//div[@class='product-list']//div[@data-test-id='price-prev-price-discount']")
+    sales = list(map(lambda x: int(sub("\\D+", "", x.text)), sales_divs))
+    print(sales)
+
+    return sales
+
+
+def get_ratings(url: str, title: str, driver) -> tuple[list[int], list[int]]:
+    driver.get(url)
+    assert title in driver.title
+
+    # Each rating has 5 uls which all have one div
+    ratings_divs = driver.find_elements(
+        By.XPATH, "//div[@class='product-list']//ul[@data-baseweb='star-rating']/li/div")
+    ratings = []
+
+    for i in range(0, len(ratings_divs)):  # Compact those divs into ratings
+        _ = ratings_divs[i].get_attribute("width")
+        if _ is not None:
+            if len(ratings) <= i // 5:
+                ratings.append(float(_.replace("%", "")) / 5)
+            else:
+                ratings[i // 5] += float(_.replace("%", "")) / 5
+
+    return ratings, get_reviews(url, title, driver)
+
+
+driver = webdriver.Firefox()
+ratings, reviews = get_ratings(
+    "https://www.hepsiburada.com/ara?q=bilgisayar&siralama=degerlendirmepuani", "Hepsiburada", driver)
+print(ratings)
+print(reviews)
+
+total = sum(reviews)
+weighted_ratings = [i * reviews[i] / total for i in ratings]
+
+fig, ax = plt.subplots()
+ax.plot
+
+# ratings = np.array(ratings)
+# reviews = np.array(reviews)
+# m, b = np.polyfit(range(0, len(ratings)), ratings, 1)
+
+# fig, ax = plt.subplots()
+# ax.scatter(range(0, len(ratings)), ratings)
+# ax.plot(range(0, len(ratings)), m*ratings + b)
+# plt.show()
+driver.close()
--- tests/sortingTests.py
+++ tests/sortingTests.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(r"C:\\GitHub Repos\\testing-project-2024")
 
 import unittest
-from src.sortLib import get_prices
+from src.sortLib import get_prices, get_reviews, get_sales
 from selenium import webdriver
 from parameterized import parameterized
 
@@ -24,11 +24,75 @@ class TestSorting(unittest.TestCase):
             ),
         }
     )
-    def testSortPriceDesc(self, url, title):
+    @unittest.skip("penis")
+    def testSortHigh(self, url, title):
         prices = get_prices(url, title, self.driver)
         flag = True
         for i in range(0, len(prices) - 1):
             if prices[i] < prices[i + 1]:
+                flag = False
+        self.assertTrue(flag)
+
+    @parameterized.expand(
+        {
+            (
+                "https://www.hepsiburada.com/ara?q=bilgisayar&siralama=artanfiyat",
+                "Hepsiburada",
+            ),
+            (
+                "https://www.hepsiburada.com/ara?q=çanta&siralama=artanfiyat",
+                "Hepsiburada",
+            ),
+        }
+    )
+    @unittest.skip("penis")
+    def testSortLow(self, url, title):
+        prices = get_prices(url, title, self.driver)
+        flag = True
+        for i in range(0, len(prices) - 1):
+            if prices[i] > prices[i + 1]:
+                flag = False
+        self.assertTrue(flag)
+
+    @parameterized.expand(
+        {
+            (
+                "https://www.hepsiburada.com/ara?q=bilgisayar&siralama=yorumsayisi",
+                "Hepsiburada",
+            ),
+            (
+                "https://www.hepsiburada.com/ara?q=çanta&siralama=yorumsayisi",
+                "Hepsiburada",
+            ),
+        }
+    )
+    @unittest.skip("penis")
+    def testSortReviewsHigh(self, url, title):
+        reviews = get_reviews(url, title, self.driver)
+        flag = True
+        for i in range(0, len(reviews) - 1):
+            if reviews[i] < reviews[i + 1]:
+                flag = False
+        self.assertTrue(flag)
+
+    @parameterized.expand(
+        {
+            (
+                "https://www.hepsiburada.com/ara?q=bilgisayar&siralama=indirimurunler",
+                "Hepsiburada",
+            ),
+            (
+                "https://www.hepsiburada.com/ara?q=çanta&siralama=indirimurunler",
+                "Hepsiburada",
+            ),
+        }
+    )
+    @unittest.skip("penis")
+    def testSortSalesHigh(self, url, title):
+        sales = get_sales(url, title, self.driver)
+        flag = True
+        for i in range(0, len(sales) - 1):
+            if sales[i] < sales[i + 1]:
                 flag = False
         self.assertTrue(flag)
 
